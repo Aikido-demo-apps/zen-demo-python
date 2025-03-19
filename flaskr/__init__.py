b1986788756c41f85f00dd27180ddee64898f893
Samuel <samuel@sava.be>
2025-03-19 08:58:33 +0100

    limit request types

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -45,39 +45,39 @@ def create_app(test_config=None):
     #     pass
 
     # Routes
-    @app.route('/')
+    @app.route('/', methods=['GET'])
     def home():
         return render_template('index.html')
 
-    @app.route('/pages/execute')
+    @app.route('/pages/execute', methods=['GET'])
     def pages_execute():
         return render_template('execute_command.html')
 
-    @app.route('/pages/create')
+    @app.route('/pages/create', methods=['GET'])
     def pages_create():
         return render_template('create.html')
 
-    @app.route('/pages/request')
+    @app.route('/pages/request', methods=['GET'])
     def pages_request():
         return render_template('request.html')
 
-    @app.route('/pages/read')
+    @app.route('/pages/read', methods=['GET'])
     def pages_read():
         return render_template('read_file.html')
 
-    @app.route('/test_ratelimiting_1')
+    @app.route('/test_ratelimiting_1', methods=['GET'])
     def test_ratelimiting_1():
         return "Request successful (Ratelimiting 1)"
 
-    @app.route('/test_ratelimiting_2')
+    @app.route('/test_ratelimiting_2', methods=['GET'])
     def test_ratelimiting_2():
         return "Request successful (Ratelimiting 2)"
 
-    @app.route('/test_bot_blocking')
+    @app.route('/test_bot_blocking', methods=['GET'])
     def test_bot_blocking():
         return "Hello World! Bot blocking enabled on this route."
 
-    @app.route('/test_user_blocking')
+    @app.route('/test_user_blocking', methods=['GET'])
     def test_user_blocking():
         return "Hello User with id: %s" % (request.headers.get('user'))
 
@@ -146,7 +146,7 @@ def create_app(test_config=None):
         return content
 
     # Static files
-    @app.route('/<path:path>')
+    @app.route('/<path:path>', methods=['GET'])
     def send_report(path):
         print(path)
         # Using request args for path will expose you to directory traversal attacks
